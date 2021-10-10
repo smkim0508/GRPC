ba4ce7d3a276b7a8a9026237f34dacccbf96ede1
Sungmin Kim <sungmin@Sungmins-MBP.fios-router.home>
2021-10-09 23:48:20 -0400

    update client

--- client.py
+++ client.py
@@ -6,6 +6,21 @@ import glob
 import cv2
 import argparse
 
+def request(ip, port, frame):
+
+    print('request: client -> server : {}{}'.format(ip,port))
+    with grpc.insecure_channel(ip+port) as channel:
+        stub = proto_sample_pb2_grpc.AI_ModelServiceStub(channel)
+        response = stub.process(
+            proto_sample_pb2.InferenceRequest(
+                img_bytes=bytes(frame),
+                width=frame.shape[1],
+                height=frame.shape[0],
+                channel=frame.shape[2]
+            )
+        )
+        return response
+
 def opt():
     parser = argparse.ArgumentParser()
     parser.add_argument('--port', type=str, default='50051', help='port number (default: 50051)')
@@ -14,6 +29,17 @@ def opt():
     
 def main():
     args = opt()
+
+    image_list = ['images.jpeg', 'images.png']
+
+    for idx, file_name in enumerate(image_list):
+        img = cv2.imread(file_name)
+
+        response = request(args.ip, args.port, img)
+
+        print(response.class_index)
+        print(response.prob)
+
 
 if __name__ == '__main__':
     main()
